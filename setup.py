d27604be62eab0ec6ff3b29dfd5535031bc47acd
yig319 <yig319@lehigh.edu>
2024-01-23 23:50:10 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Wallpaper_Group_Dataset',
+    version='0.1',
+    author='Yichen Guo, Joshua Agar',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
