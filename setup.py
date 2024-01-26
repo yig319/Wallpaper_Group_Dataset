874aa512fe313a239895b4f400ef93195f39a62b
Yichen Guo <yig319@lehigh.edu>
2024-01-26 16:04:45 +0000

    build pip

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name='Wallpaper_Group_Dataset',
     version='0.1',
     author='Yichen Guo, Joshua Agar',
+    url='https://github.com/yig319/Wallpaper_Group_Dataset.git',
     packages=find_packages(),
-    install_requires=[
-        # List your package dependencies here
-    ],
 )
